9f5be7853e58b2f703f45de95c8ceeb83c261f93
Portevin Grégoire <gportevin@gaming.tech>
2025-06-16 15:00:13 +0200

    Playerclass

---
+++ Smash/Assets/Script/Foot.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Foot : MonoBehaviour
+{
+    public PlayerController controller;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            controller.numJump = 3;
+        }
+    }
+}
