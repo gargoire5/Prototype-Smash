2d906cf933e3c91b50ad090a30926887b7dd2381
Portevin Grégoire <gportevin@gaming.tech>
2025-06-18 14:40:43 +0200

    PP

---
+++ Smash/Assets/Script/Characters/CHA_Greg.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CHA_Greg : CharacterAttack
+{
+    [SerializeField]
+    private GameObject ultObject;
+
+    [SerializeField]
+    private GameObject skillObject;
+
+    private void Start()
+    {
+        //Fields to modify when making a character
+
+        basicAttackDuration = 0.2f;
+        basicAttackDelay = 0.1f;
+        basicAttackRate = 0.2f;
+        basicAttackDamage = 5.0f;
+
+        chargeAttackDuration = 0.2f;
+        chargeAttackDelay = 0.0f;
+        chargeAttackRate = 0.2f;
+        chargeAttackDamage = 7.5f;
+        chargeTimeTreshold = 0.5f;
+
+        skillDuration = 0.2f;
+        skillDelay = 0.0f;
+        skillRate = 3.0f;
+        skillDamage = 10.0f;
+
+        ultimateDuration = 1f;
+        ultimateDelay = 0.0f;
+        ultimateRate = 30.0f;
+        ultimateDamage = 20.0f;
+    }
+
+    protected override void BasicAttack()
+    {
+        base.BasicAttack();
+        Debug.Log("BasicAttack bleb");
+    }
+
+    protected override void ChargeAttack()
+    {
+        Debug.Log("ChargeAttack");
+    }
+
+    protected override void SkillAttack()
+    {
+        base.SkillAttack();
+
+        GameObject currentSkill = Instantiate(skillObject, selectedHitbox.transform.position, selectedHitbox.transform.rotation);
+        SKI_Fugue skill = currentSkill.GetComponent<SKI_Fugue>();
+
+        if (selectedHitbox == hitboxLeft)
+            skill.SetDirection(-1);
+        Destroy(currentSkill, 2.0f);
+    }
+
+    protected override void UltimateAttack()
+    {
+        base.UltimateAttack();
+
+        GameObject selectedHitbox = ultObject;
+
+        StartCoroutine(UltAttack(selectedHitbox));
+    }
+
+    protected override void ParadeAction()
+    {
+
+    }
+
+    private IEnumerator UltAttack(GameObject hitbox)
+    {
+        yield return new WaitForSeconds(ultimateDelay);
+
+        GameObject currentHitbox = Instantiate(hitbox, transform.position, transform.rotation);
+
+        yield return new WaitForSeconds(ultimateDuration);
+
+        Destroy(currentHitbox);
+    }
+}
