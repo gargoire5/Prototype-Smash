8f33a3af5c7345bfd95b1abf68f4d15d86697179
ManaIron <117281679+ManaIron@users.noreply.github.com>
2025-06-18 15:53:10 +0200

    haha

--- Smash/Assets/Script/Characters/CHA_Manaarii.cs
+++ Smash/Assets/Script/Characters/CHA_Manaarii.cs
@@ -3,31 +3,13 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class CHA_Fugue : CharacterAttack
+public class CHA_Manaarii : CharacterAttack
 {
     [SerializeField]
     private GameObject ultObject;
 
     [SerializeField]
     private GameObject skillObject;
-
-    /*public override float BasicDamage => 6f;
-    public override float BasicKnockback => 5f;
-    public override float BasicDuration => 0.4f;
-    public override float BasicDelay => 0.05f;
-    public override float BasicRate => 0.1f;
-
-    public override float SkillDamage => 18f;
-    public override float SkillKnockback => 12f;
-    public override float SkillDuration => 0f;
-    public override float SkillDelay => 0f;
-    public override float SkillRate => 3f;
-
-    public override float UltimateDamage => 35f;
-    public override float UltimateKnockback => 40f;
-    public override float UltimateDuration => 0.5f;
-    public override float UltimateDelay => 0f;
-    public override float UltimateRate => 30f;*/
 
     private void Start()
     {
@@ -58,34 +40,31 @@ public class CHA_Fugue : CharacterAttack
     protected override void BasicAttack()
     {
         base.BasicAttack();
-        Debug.Log("BasicAttack bleb");
     }
 
     protected override void ChargeAttack()
     {
-        Debug.Log("ChargeAttack");
+
     }
 
     protected override void SkillAttack()
     {
-        if (canUseSkill)
-        {
-            GameObject currentSkill = Instantiate(skillObject, selectedHitbox.transform.position, selectedHitbox.transform.rotation);
-            SKI_Fugue skill = currentSkill.GetComponent<SKI_Fugue>();
+        base.SkillAttack();
 
-            if (selectedHitbox == hitboxLeft)
-                skill.SetDirection(-1);
-            Destroy(currentSkill, 2.0f);
+        if (selectedHitbox == null)
+            selectedHitbox = hitboxRight;
 
-            StartCoroutine(UseSkill());
-        }
+        GameObject currentSkill = Instantiate(skillObject, selectedHitbox.transform.position, selectedHitbox.transform.rotation);
+        SKI_Fugue skill = currentSkill.GetComponent<SKI_Fugue>();
+
+        if (selectedHitbox == hitboxLeft)
+            skill.SetDirection(-1);
+        Destroy(currentSkill, 2.0f);
     }
 
-   protected override void UltimeAttack()
+    protected override void UltimateAttack()
     {
-        if (!canUseUltimate) return;
-
-        Debug.Log("Ultimate");
+        base.UltimateAttack();
 
         GameObject selectedHitbox = ultObject;
 
